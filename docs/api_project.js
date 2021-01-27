64e214db2e9b09975c19509ffadbb7c96243d512
Valentin Perignon <valentin.perignon@orange.fr>
2021-01-27 18:18:05 +0100

    Update user

--- docs/api_project.js
+++ docs/api_project.js
@@ -18,7 +18,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-01-27T12:31:10.196Z",
+    "time": "2021-01-27T17:17:43.076Z",
     "url": "https://apidocjs.com",
     "version": "0.26.0"
   }
