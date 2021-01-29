a6a1634d20427c6cdb4c3bcd042e95a754854497
Nathanaël Houn <contact@nathanaelhoun.fr>
2021-01-30 00:45:59 +0100

    Updating API Documentation

--- docs/api_project.js
+++ docs/api_project.js
@@ -18,7 +18,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-01-28T17:33:00.523Z",
+    "time": "2021-01-29T23:45:25.912Z",
     "url": "https://apidocjs.com",
     "version": "0.26.0"
   }
